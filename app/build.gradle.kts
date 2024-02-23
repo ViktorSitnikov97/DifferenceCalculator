f65b1f9e3717521b09d37f6020712ec32464bf7a
Viktor Sitnikov <sitnikoff_victor_97@mail.ru>
2024-02-23 21:49:41 +0300

    add app

---
+++ app/build.gradle.kts
@@ -0,0 +1,38 @@
+import org.gradle.api.tasks.testing.logging.TestExceptionFormat
+import org.gradle.api.tasks.testing.logging.TestLogEvent
+
+plugins {
+    id("java")
+    application
+    jacoco
+    id("checkstyle")
+    id("io.freefair.lombok") version "8.4"
+    id("com.github.ben-manes.versions") version "0.50.0"
+}
+
+group = "hexlet.code"
+version = "1.0-SNAPSHOT"
+
+//application { mainClass.set("hexlet.code.App") }
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.apache.commons:commons-lang3:3.14.0")
+    implementation("org.apache.commons:commons-collections4:4.4")
+    testImplementation(platform("org.junit:junit-bom:5.9.1"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+    testLogging {
+        exceptionFormat = TestExceptionFormat.FULL
+        events = mutableSetOf(TestLogEvent.FAILED, TestLogEvent.PASSED, TestLogEvent.SKIPPED)
+        // showStackTraces = true
+        // showCauses = true
+        showStandardStreams = true
+    }
+}
