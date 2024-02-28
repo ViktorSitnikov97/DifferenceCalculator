e45f0bf5683fa6d668fcb825e5eac766160dd63f
Viktor Sitnikov <sitnikoff_victor_97@mail.ru>
2024-02-28 17:11:31 +0300

    fix App.java

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -4,31 +4,38 @@ import picocli.CommandLine;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
 import picocli.CommandLine.Parameters;
-import java.io.IOException;
-import java.io.File;
 import java.util.concurrent.Callable;
 
 @Command(name = "gendiff",
         mixinStandardHelpOptions = true,
         version = "App 1.0",
         description = "Compares two configuration files and shows a difference.")
-class App implements Callable<String> {
+class App implements Callable<Integer> {
+    private static final int SUCCESS_EXIT = 0;
+    private static final int ERROR_EXIT = 1;
 
     @Parameters(paramLabel = "filepath1", description = "path to first file")
-    File files1;
+    private String filePath1;
     @Parameters(paramLabel = "filepath2", description = "path to second file")
-    File files2;
-    @Option(names = {"-f", "--format"}, paramLabel = "format", description = "output format [default: stylish]")
-    String format;
+    private String filePath2;
+    @Option(names = {"-f", "--format"},
+            paramLabel = "format",
+            description = "output format [default: stylish]")
+    private String format;
 
     @Override
-    public String call() throws IOException {
-//        Differ.generate(files1, files2);
-        return "something";
-
+    public Integer call() throws Exception {
+        try {
+            String formattedDiff = Differ.generate(filePath1, filePath2);
+            System.out.println(formattedDiff);
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+            return ERROR_EXIT;
+        }
+        return SUCCESS_EXIT;
     }
 
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws Exception {
         int exitCode = new CommandLine(new App()).execute(args);
         System.exit(exitCode);
     }
