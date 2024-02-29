e5d8ffc6366c54ec9e9027bc199d71a3324aab0c
Viktor Sitnikov <sitnikoff_victor_97@mail.ru>
2024-02-29 21:56:36 +0300

    refactoring DifferTest.java

--- app/src/test/java/hexlet/code/DifferTest.java
+++ app/src/test/java/hexlet/code/DifferTest.java
@@ -1,17 +1,13 @@
 package hexlet.code;
 
 import org.junit.jupiter.api.Test;
-
 import java.io.File;
-import java.io.IOException;
-
 import static hexlet.code.Differ.generate;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class DifferTest {
     @Test
-    public void generateTest() throws IOException {
+    public void generateTest1() throws Exception {
         String expected = "{\n"
                 + "  - follow: false\n"
                 + "    host: hexlet.io\n"
@@ -21,14 +17,31 @@ public class DifferTest {
                 + "  + verbose: true\n"
                 + "}";
 
-//        String path = "src/test/resources/fixtures";
-//        File file = new File(path);
-//        String absolutePath = file.getAbsolutePath();
-//        System.out.println(absolutePath);
-//        assertTrue(absolutePath.endsWith("src/test/resources/fixtures"));
-
         String filePath1 = "src/test/resources/fixtures/file1.json";
         String filePath2 = "src/test/resources/fixtures/file2.json";
+        File file1 = new File(filePath1);
+        File file2 = new File(filePath2);
+        String absolutePath1 = file1.getAbsolutePath();
+        String absolutePath2 = file2.getAbsolutePath();
+
+        String actual = generate(absolutePath1, absolutePath2);
+        System.out.println(actual);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void generateTest2() throws Exception {
+        String expected = "{\n"
+                + "  - follow: false\n"
+                + "    host: hexlet.io\n"
+                + "  - proxy: 123.234.53.22\n"
+                + "  - timeout: 50\n"
+                + "  + timeout: 20\n"
+                + "  + verbose: true\n"
+                + "}";
+
+        String filePath1 = "src/test/resources/fixtures/file1.yml";
+        String filePath2 = "src/test/resources/fixtures/file2.yml";
         File file1 = new File(filePath1);
         File file2 = new File(filePath2);
         String absolutePath1 = file1.getAbsolutePath();
