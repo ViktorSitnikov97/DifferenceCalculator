854f5bae9d5454bf04f4ee2be2dffa903ce6fcee
Viktor Sitnikov <sitnikoff_victor_97@mail.ru>
2024-02-29 21:57:45 +0300

    add Parser.java

---
+++ app/src/main/java/hexlet/code/Parser.java
@@ -0,0 +1,34 @@
+package hexlet.code;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLMapper;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.TreeMap;
+
+public class Parser {
+    public static TreeMap<String, Object> parse(String filePath) throws Exception {
+
+        int index = filePath.lastIndexOf('.');
+        String format = index > 0 ? filePath.substring(index + 1) : "";
+        Path path = Paths.get(filePath).toAbsolutePath().normalize();
+        String content = Files.readString(path);
+
+        switch (format) {
+            case "json":
+                ObjectMapper mapperJson = new ObjectMapper();
+                return mapperJson.readValue(content,
+                        new TypeReference<TreeMap<String, Object>>() {
+                        });
+            case "yml":
+                ObjectMapper mapperYaml = new YAMLMapper();
+                return mapperYaml.readValue(content,
+                        new TypeReference<TreeMap<String, Object>>() {
+                        });
+            default:
+                throw new Exception("Wrong format: '" + format + "'");
+        }
+    }
+}
