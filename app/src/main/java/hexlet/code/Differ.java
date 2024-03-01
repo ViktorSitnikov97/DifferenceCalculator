bdc1d54caf245268fe6210a3fd1e18078028c9d4
Viktor Sitnikov <sitnikoff_victor_97@mail.ru>
2024-03-01 15:10:41 +0300

    refactoring Differ.java

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -1,8 +1,6 @@
 package hexlet.code;
 
 import java.util.TreeMap;
-import hexlet.code.formats.Stylish;
-import hexlet.code.formats.Plain;
 public class Differ {
     public static String generate(String filePath1, String filePath2, String format) throws Exception {
 
@@ -10,13 +8,7 @@ public class Differ {
         TreeMap<String, Object> mapFromFile2 = Parser.parse(filePath2);
         TreeMap<String, Status> resultMap = CalculateDifference.getDifference(mapFromFile1, mapFromFile2);
 
-        if (format.equals("stylish")) {
-            return Stylish.getReport(resultMap);
-        } else if (format.equals("plain")) {
-            return Plain.getReport(resultMap);
-        } else {
-            return "yy";
-        }
+        return Formatter.selectFormat(resultMap, format);
     }
     public static String generate(String filePath1, String filePath2) throws Exception {
         return generate(filePath1, filePath2, "stylish");
