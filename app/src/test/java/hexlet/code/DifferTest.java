f6633f60995fc0aaef9baf35da3b0068afa00f84
Viktor Sitnikov <sitnikoff_victor_97@mail.ru>
2024-03-01 22:41:26 +0300

    refactoring DifferTest.java

--- app/src/test/java/hexlet/code/DifferTest.java
+++ app/src/test/java/hexlet/code/DifferTest.java
@@ -1,118 +1,130 @@
 package hexlet.code;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import java.io.File;
-import static hexlet.code.Differ.generate;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class DifferTest {
-    @Test
-    public void generateTest1() throws Exception {
-        String expected = "{\n"
-                + "  - follow: false\n"
-                + "    host: hexlet.io\n"
-                + "  - proxy: 123.234.53.22\n"
-                + "  - timeout: 50\n"
-                + "  + timeout: 20\n"
-                + "  + verbose: true\n"
-                + "}";
 
-        String filePath1 = "src/test/resources/fixtures/file1.json";
-        String filePath2 = "src/test/resources/fixtures/file2.json";
-        File file1 = new File(filePath1);
-        File file2 = new File(filePath2);
-        String absolutePath1 = file1.getAbsolutePath();
-        String absolutePath2 = file2.getAbsolutePath();
+    private static String stylishComparisonResult;
+    private static String jsonComparisonResult;
+    private static String plainComparisonResult;
 
-        String actual = generate(absolutePath1, absolutePath2);
-        System.out.println(actual);
-        assertEquals(expected, actual);
+
+    private static Path getPath(String fileName) {
+        return Paths.get("src", "test", "resources", "fixtures", fileName)
+                .toAbsolutePath().normalize();
+    }
+
+    private static String getDataString(String fileName) throws Exception {
+        Path currentPath = getPath(fileName);
+        return Files.readString(currentPath);
+    }
+
+    @BeforeAll
+    public static void beforeAll() throws Exception {
+        stylishComparisonResult = getDataString("StylishComparisonResultDeepStructure.txt");
+        plainComparisonResult = getDataString("PlainComparisonResultDeepStructure.txt");
+        jsonComparisonResult = getDataString("JsonComparisonResultDeepStructure.json");
     }
 
     @Test
-    public void generateTest2() throws Exception {
-        String expected = "Property 'follow' was removed\n"
-                + "Property 'proxy' was removed\n"
-                + "Property 'timeout' was updated. From 50 to 20\n"
-                + "Property 'verbose' was added with value: true";
+    public void generateTestStylishForFilesJson() throws Exception {
 
-        String filePath1 = "src/test/resources/fixtures/file1.yml";
-        String filePath2 = "src/test/resources/fixtures/file2.yml";
-        File file1 = new File(filePath1);
-        File file2 = new File(filePath2);
-        String absolutePath1 = file1.getAbsolutePath();
-        String absolutePath2 = file2.getAbsolutePath();
+        Path file1 = getPath("DeepStructurefile1.json");
+        Path file2 = getPath("DeepStructurefile2.json");
 
-        String actual = generate(absolutePath1, absolutePath2, "plain");
-        System.out.println(actual);
-        assertEquals(expected, actual);
+        String absolutePathFile1 = file1.toFile().getAbsolutePath();
+        String absolutePathFile2 = file2.toFile().getAbsolutePath();
+
+        String actual = Differ.generate(absolutePathFile1, absolutePathFile2);
+        assertEquals(stylishComparisonResult, actual);
+
     }
 
     @Test
-    public void generateTest3() throws Exception {
-        String expected = "{\n"
-                + "    chars1: [a, b, c]\n"
-                + "  - chars2: [d, e, f]\n"
-                + "  + chars2: false\n"
-                + "  - checked: false\n"
-                + "  + checked: true\n"
-                + "  - default: null\n"
-                + "  + default: [value1, value2]\n"
-                + "  - id: 45\n"
-                + "  + id: null\n"
-                + "  - key1: value1\n"
-                + "  + key2: value2\n"
-                + "    numbers1: [1, 2, 3, 4]\n"
-                + "  - numbers2: [2, 3, 4, 5]\n"
-                + "  + numbers2: [22, 33, 44, 55]\n"
-                + "  - numbers3: [3, 4, 5]\n"
-                + "  + numbers4: [4, 5, 6]\n"
-                + "  + obj1: {nestedKey=value, isNested=true}\n"
-                + "  - setting1: Some value\n"
-                + "  + setting1: Another value\n"
-                + "  - setting2: 200\n"
-                + "  + setting2: 300\n"
-                + "  - setting3: true\n"
-                + "  + setting3: none\n"
-                + "}";
+    public void generateTestStylishForFilesYml() throws Exception {
 
-        String filePath1 = "src/test/resources/fixtures/file3.json";
-        String filePath2 = "src/test/resources/fixtures/file4.json";
-        File file1 = new File(filePath1);
-        File file2 = new File(filePath2);
-        String absolutePath1 = file1.getAbsolutePath();
-        String absolutePath2 = file2.getAbsolutePath();
+        Path file1 = getPath("DeepStructurefile1.yml");
+        Path file2 = getPath("DeepStructurefile2.yml");
 
-        String actual = generate(absolutePath1, absolutePath2);
-        System.out.println(actual);
-        assertEquals(expected, actual);
+        String absolutePathFile1 = file1.toFile().getAbsolutePath();
+        String absolutePathFile2 = file2.toFile().getAbsolutePath();
+
+        String actual = Differ.generate(absolutePathFile1, absolutePathFile2);
+        assertEquals(stylishComparisonResult, actual);
+
     }
 
     @Test
-    public void generateTest4() throws Exception {
-        String expected = "Property 'chars2' was updated. From [complex value] to false\n"
-                + "Property 'checked' was updated. From false to true\n"
-                + "Property 'default' was updated. From null to [complex value]\n"
-                + "Property 'id' was updated. From 45 to null\n"
-                + "Property 'key1' was removed\n"
-                + "Property 'key2' was added with value: 'value2'\n"
-                + "Property 'numbers2' was updated. From [complex value] to [complex value]\n"
-                + "Property 'numbers3' was removed\n"
-                + "Property 'numbers4' was added with value: [complex value]\n"
-                + "Property 'obj1' was added with value: [complex value]\n"
-                + "Property 'setting1' was updated. From 'Some value' to 'Another value'\n"
-                + "Property 'setting2' was updated. From 200 to 300\n"
-                + "Property 'setting3' was updated. From true to 'none'";
+    public void generateTestPlainForFilesJson() throws Exception {
 
-        String filePath1 = "src/test/resources/fixtures/file3.yml";
-        String filePath2 = "src/test/resources/fixtures/file4.yml";
-        File file1 = new File(filePath1);
-        File file2 = new File(filePath2);
-        String absolutePath1 = file1.getAbsolutePath();
-        String absolutePath2 = file2.getAbsolutePath();
+        Path file1 = getPath("DeepStructurefile1.json");
+        Path file2 = getPath("DeepStructurefile2.json");
 
-        String actual = generate(absolutePath1, absolutePath2, "plain");
-        System.out.println(actual);
-        assertEquals(expected, actual);
+        String absolutePathFile1 = file1.toFile().getAbsolutePath();
+        String absolutePathFile2 = file2.toFile().getAbsolutePath();
+
+        String actual = Differ.generate(absolutePathFile1, absolutePathFile2, "plain");
+        assertEquals(plainComparisonResult, actual);
+
     }
+
+    @Test
+    public void generateTestPlainForFilesYml() throws Exception {
+
+        Path file1 = getPath("DeepStructurefile1.yml");
+        Path file2 = getPath("DeepStructurefile2.yml");
+
+        String absolutePathFile1 = file1.toFile().getAbsolutePath();
+        String absolutePathFile2 = file2.toFile().getAbsolutePath();
+
+        String actual = Differ.generate(absolutePathFile1, absolutePathFile2, "plain");
+        assertEquals(plainComparisonResult, actual);
+
+    }
+
+    @Test
+    public void generateTestJsonForFilesJson() throws Exception {
+
+        Path file1 = getPath("DeepStructurefile1.json");
+        Path file2 = getPath("DeepStructurefile2.json");
+
+        String absolutePathFile1 = file1.toFile().getAbsolutePath();
+        String absolutePathFile2 = file2.toFile().getAbsolutePath();
+
+        String actual = Differ.generate(absolutePathFile1, absolutePathFile2, "json");
+        assertEquals(jsonComparisonResult, actual);
+
+    }
+
+    @Test
+    public void generateTestJsonForFilesYml() throws Exception {
+
+        Path file1 = getPath("DeepStructurefile1.yml");
+        Path file2 = getPath("DeepStructurefile2.yml");
+
+        String absolutePathFile1 = file1.toFile().getAbsolutePath();
+        String absolutePathFile2 = file2.toFile().getAbsolutePath();
+
+        String actual = Differ.generate(absolutePathFile1, absolutePathFile2, "json");
+        assertEquals(jsonComparisonResult, actual);
+
+    }
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
